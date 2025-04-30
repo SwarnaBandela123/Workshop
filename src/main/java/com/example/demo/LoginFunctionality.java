77dea02f8a10fab65dec194a67a1c5e37d638c08
Swarna <swarna.bandela@cognine.com>
2025-04-30 15:25:03 +0530

    second commit

--- src/main/java/com/example/demo/LoginFunctionality.java
+++ src/main/java/com/example/demo/LoginFunctionality.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LoginFunctionality {
 
+	
 	public static void main(String[] args) {
         SpringApplication.run(LoginFunctionality.class, args);
     }
